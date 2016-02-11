0dcb1b77e49d61c424a24244be5d4c470b179365
Sunny Shah <shahsunny712@gmail.com>
2016-02-10 19:36:34 -0600

    Initial commit - blank activity with no fragmnents or menus
    Target Android Version - 5.1 (API level 22)

---
+++ app/src/androidTest/java/com/ssunny/cs407/hw/quizapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ssunny.cs407.hw.quizapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
